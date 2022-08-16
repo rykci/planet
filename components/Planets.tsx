3dea7b41bb60038af9e3af068167e1e772cbbccc
rykci <yuen.ricky@hotmail.com>
2022-08-16 17:04:53 -0400

    add planet function

--- components/Planets.tsx
+++ components/Planets.tsx
@@ -5,20 +5,42 @@ import Header from '../components/Header'
 
 import { auth, db } from '../firebase/firebase'
 import { signOut } from 'firebase/auth'
-import { doc, updateDoc } from 'firebase/firestore'
+import { collection, doc, updateDoc, addDoc } from 'firebase/firestore'
 import { AuthContext } from '../context/auth'
 
 function Planets() {
   const { user } = useContext(AuthContext)
   const [addingPlanet, setAddingPlanet] = useState(false)
+  const [planetName, setPlanetName] = useState('')
+
+  const createPlanet = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    try {
+      const result = await addDoc(collection(db, 'planets'), {
+        planetName,
+        members: [user.uid],
+      })
+
+      //console.log(result.id)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPlanetName(e.target.value)
+  }
+
   return (
     <main className="flex flex-col space-between max-h-screen h-screen bg-gray-100">
       <Header plusAction={() => setAddingPlanet(!addingPlanet)} />
       {addingPlanet ? (
-        <form className="flex">
+        <form className="flex" onSubmit={createPlanet}>
           <input
             className="p-3 border-2 border-y-0 flex-grow-1 w-full"
             placeholder="Planet Name"
+            onChange={handleChange}
+            value={planetName}
           />
           <button className="bg-blue-500 hover:border-blue-600 hover:bg-blue-600 border-blue-500 text-white p-3 px-6 self-end">
             Create
